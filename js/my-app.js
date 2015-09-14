b16542699602221c97be1e07d83d693467ca830e
Girish Rawat <girishrawat@Girishs-MacBook-Air.local>
2015-09-14 21:02:24 +0530

    Uploaded all the files for Appwalle

---
+++ js/my-app.js
@@ -0,0 +1,1 @@
+var myApp=new Framework7();var $$=Dom7;var view1=myApp.addView('#view-1');var view2=myApp.addView('#view-2',{dynamicNavbar:true});var view3=myApp.addView('#view-3');var view4=myApp.addView('#view-4');
